fd6ca93884e8dbdd36f2d887644a8a9218853a31
vhoreho <vpubuntu@gmail.com>
2023-01-12 19:14:33 +0300

    add static method to class Plane

--- Plane.js
+++ Plane.js
@@ -47,10 +47,19 @@ class Plane {
     }
     return `Speed: ${this.speed}, Altitude: ${this.altitude}`;
   }
+
+  static getMostFastestPlane(plane1, plane2, plane3) {
+    return plane1.speed > plane2.speed && plane1.speed > plane3.speed
+      ? plane1
+      : plane2.speed > plane3.speed && plane2.speed > plane1.speed
+      ? plane2
+      : plane3;
+  }
 }
 
-const plane1 = new Plane('B034S', '403', [10, 10, 10, 10], 3, 300, 20, 3000, 30, 100);
-const plane2 = new Plane('B034G', '464', [10, 40, 20, 10], 6, 700, 10, 2000, 40, 100);
+const Boeing = new Plane('B034S', '403', [10, 10, 10, 10], 3, 300, 20, 3000, 30, 100);
+const Corn = new Plane('B034G', '464', [10, 40, 20, 10], 6, 700, 10, 2000, 40, 100);
+const Helicopter = new Plane('B034R', '461', [10, 40, 20, 10], 6, 700, 10, 2000, 40, 100);
 
-console.log(plane1.getConsumption());
-console.log(plane1.getSpeedy(plane2));
+console.log(Plane.getMostFastestPlane(Boeing, Corn, Helicopter));
+// console.log(plane1.getSpeedy(plane2));
